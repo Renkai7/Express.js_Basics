e214f603c0c40ad9a729c17e21e4bac4883af1c8
Mike <ms53150@gmail.com>
2022-06-02 18:40:50 -0400

    Add a new route (contact) to server

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 
 // home page
-app.get("/", function (request, response) {
-	response.send("<h1>Hello, world!</h1>");
+app.get("/", function (req, res) {
+	res.send("<h1>Hello, world!</h1>");
+});
+
+app.get("/contact", function (req, res) {
+	res.send("Contact me at: contact@gmail.com");
 });
 
 app.listen(3000, function () {
